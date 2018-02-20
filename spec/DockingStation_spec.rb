7ab1f9109bea9c8bc968d1ad94c87e9589e5a2f4
Cindy <jl7e12@gmail.com>
2018-02-20 16:35:06 +0000

    added bike spec and ruby file

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,6 +2,6 @@ require 'docking_station'
 describe DockingStation do
   describe '.release_bike'
     it 'release bike on docking station' do
-      expect(DockingStation).to respond_to '.release_bike'
+      expect(DockingStation).to respond_to (:release_bike)
   end
 end
