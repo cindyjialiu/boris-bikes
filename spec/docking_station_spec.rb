d8f22e8c0ab1f4b64d408dc038d31c4cfd584aa5
Cindy <jl7e12@gmail.com>
2018-02-22 00:58:19 +0000

    added test on raising error when no bikes available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,17 @@
 require 'docking_station'
 describe DockingStation do
+
   describe '.release_bike' do
     it 'release bike on docking station' do
       docking_station = DockingStation.new
       expect(docking_station.release_bike).to be_instance_of(Bike)
     end
+
+    it 'raise error message if no bikes available' do
+      docking_station = DockingStation.new
+      expect {docking_station.release_bike}.to raise_error("Sorry, no bikes available")
+    end
+
   end
   describe '.dock' do
     it 'dock the bike' do
@@ -12,5 +19,6 @@ describe DockingStation do
       bike = Bike.new
       expect(docking_station.dock(bike)).to eq bike
     end
+
   end
 end
